dbf697d4b1e6919185dd7efd19e902f58adb70ad
Ricky <rickardobell@gmail.com>
2022-11-16 22:48:35 +0000

    Background Scrolling

--- Assets/Scripts/BG_Scroll.cs
+++ Assets/Scripts/BG_Scroll.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class BG_Scroll : MonoBehaviour
 {
+
+    //this is public so that the value can be changed if it needs to be
+    public float scroll_Speed = 0.1f;
+
+    private MeshRenderer mesh_Renderer;
+
+    private float x_Scroll;
+    
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        mesh_Renderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        Scroll();
+    } 
+    void Scroll()
+        {
+            //This is the time since the game started 
+            x_Scroll = Time.time * scroll_Speed;
+
+            Vector2 offset = new Vector2(x_Scroll, 0f);
+            mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+        }
 }
+
