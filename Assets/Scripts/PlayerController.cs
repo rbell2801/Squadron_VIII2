6e6c950a7ea68eb4b13f81dc5c3e2a59d5daab87
Ricky <rickardobell@gmail.com>
2022-11-19 21:10:17 +0000

    Up down movement finished

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+
+    public float speed = 5f;
+
+    public float min_Y, max_Y;
+
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            MovePlayer();
+        }
+        //The move player 
+        void MovePlayer()
+        {
+            
+            if (Input.GetAxisRaw("Vertical") > 0f)
+            {
+                Vector3 temp = transform.position;
+                temp.y += speed * Time.deltaTime;
+
+                //this stops the player from going out of bounds at the top of the screen
+                if (temp.y > max_Y)
+                    temp.y = max_Y;
+
+                transform.position = temp;
+            } 
+            else if (Input.GetAxisRaw("Vertical") < 0f) 
+            {
+                Vector3 temp = transform.position;
+                temp.y -= speed * Time.deltaTime;
+                
+                //this stops the player from going out of bounds at the top of the screen
+                if (temp.y < min_Y)
+                    temp.y = min_Y;
+
+                transform.position = temp;
+            }
+        }
+
+    }
